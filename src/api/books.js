ee475fc63ad1f01f408d169a55c91bdabf30f33a
Ruslan Korolev <great1672@gmail.com>
2020-07-22 18:49:33 +0300

    setup API HOST in .env

--- src/api/books.js
+++ src/api/books.js
@@ -1,24 +1,25 @@
 import axios from 'axios'
+axios.defaults.baseURL = process.env.REACT_APP_API_HOST;
 
 export default {
   index(params={}) {
-    return axios.get('http://localhost:4000/books', { params })
+    return axios.get('/books', { params })
       .then(response => response.data)
   },
   create(book) {
-    return axios.post('http://localhost:4000/books', { book })
+    return axios.post('/books', { book })
       .then(response => response.data)
   },
   update(id, { title }) {
-    return axios.patch('http://localhost:4000/books/' + id, { title })
+    return axios.patch('/books/' + id, { title })
       .then(response => response.data)
   },
   read(id) {
-    return axios.get('http://localhost:4000/books/' + id)
+    return axios.get('/books/' + id)
       .then(response => response.data)
   },
   delete(id) {
-    return axios.delete('http://localhost:4000/books/' + id)
+    return axios.delete('/books/' + id)
   }
 }
 
