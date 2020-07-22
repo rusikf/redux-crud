8ef0ef60287e7fd69a563ad482c9544d065b6a0a
Ruslan Korolev <great1672@gmail.com>
2020-07-22 19:07:07 +0300

    Updates

---
+++ precache-manifest.993e3d1bfc86746098c9b684328a82c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9802fb66d7b0571bb826b69450a3219",
+    "url": "/redux-crud/index.html"
+  },
+  {
+    "revision": "7f7f9e2439dfd8fd536c",
+    "url": "/redux-crud/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "05dac05cb2746bd29276",
+    "url": "/redux-crud/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7f7f9e2439dfd8fd536c",
+    "url": "/redux-crud/static/js/2.57bc9ebc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-crud/static/js/2.57bc9ebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05dac05cb2746bd29276",
+    "url": "/redux-crud/static/js/main.345f3f70.chunk.js"
+  },
+  {
+    "revision": "d93abe5442734fb667f7",
+    "url": "/redux-crud/static/js/runtime-main.ec73f392.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/redux-crud/precache-manifest.993e3d1bfc86746098c9b684328a82c2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/redux-crud/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
